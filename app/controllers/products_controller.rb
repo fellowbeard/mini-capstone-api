fdd88912e06ec8f44040583800838400b824b719
fellowbeard <virgovibe@gmail.com>
2022-03-08 16:40:34 -1000

    fixed errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
+
   def one_product
     one_product = Product.first
-    render json: "Hi"
+    render json: one_product.as_json
   end
 
   def all_products
     all_products = Product.all
-    render json: "hi"
+    render json: all_products.as_json
   end
+
 end
