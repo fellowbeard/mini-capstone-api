9ce1572b50d631c2cc5c95bb6320a301cd2f39db
fellowbeard <virgovibe@gmail.com>
2022-03-10 14:35:08 -1000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  get "/one_product", controller: "products", action: "one_product"
-  get "/all_products", controller: "products", action: "all_products"
+  get "/one_product" => "products#one_product"
+
+  get "/all_products" => "products#all_products"
+
+  get "/all_products/:price" => "products#all_products"
 
 end
